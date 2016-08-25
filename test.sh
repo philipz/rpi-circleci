f63303ef21b659da3f490a8074d81c1862188bdf
philipz <philipzheng@gmail.com>
2016-08-25 16:32:29 +0800

    Fix test.sh again.

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm --name rpi --network rpicircleci_mqtt philipz/rpi-raspbian-mqtt qemu-arm-static /bin/sh -c 'mosquitto_pub -h localhost -t /test -m "Hello World, Philipz!'
+docker run --network=rpicircleci_mqtt -ti --rm  philipz/rpi-raspbian-mqtt qemu-arm-static /bin/sh -c 'mosquitto_pub -h localhost -t /test -m "Hello World, Philipz!'
