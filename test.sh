042064048323ab381031f3ea7525bfcf68e1ef9e
philipz <philipzheng@gmail.com>
2016-08-25 13:47:55 +0800

    Fix test.sh & docker-compose again.

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm --name rpi -e QEMU_EXECVE=1 -v $(pwd)/qemu/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/qemu/sh-shim:/usr/bin/sh-shim -v $(pwd)/qemu/cross-build-start:/usr/bin/cross-build-start philipz/rpi-raspbian-mqtt mosquitto_pub -h localhost -t /test -m "Hello World, Philipz!"
+docker run -ti --rm --name rpi -e QEMU_EXECVE=1 -v $(pwd)/qemu/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/qemu/sh-shim:/usr/bin/sh-shim -v $(pwd)/qemu/cross-build-start:/usr/bin/cross-build-start philipz/rpi-raspbian-mqtt cross-build-start && /usr/bin/mosquitto_pub -h localhost -t /test -m "Hello World, Philipz!"
