37ccc037a05510c887e9a3916e5161fcfcc68d68
philipz <philipzheng@gmail.com>
2016-08-25 12:14:15 +0800

    Fix test.sh.

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm --name rpi -e QEMU_EXECVE=1 -v $(pwd)/qemu/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/qemu/sh-shim:/usr/bin/sh-shim -v $(pwd)/qemu/cross-build-start:/usr/bin/cross-build-start philipz/rpi-raspbian cross-build-start && mosquitto_pub -h localhost -t /test -m "Hello World, Philipz!"
+docker run -ti --rm --name rpi -e QEMU_EXECVE=1 -v ./qemu/qemu-arm-static:/usr/bin/qemu-arm-static -v ./qemu/sh-shim:/usr/bin/sh-shim -v ./qemu/cross-build-start:/usr/bin/cross-build-start philipz/rpi-raspbian-mqtt cross-build-start && mosquitto_pub -h localhost -t /test -m "Hello World, Philipz!"
